83cb983b98d551e51ef2cad7bae8fda6703ca554
hariprasad2512 <111779312+hariprasad2512@users.noreply.github.com>
2023-01-11 00:43:53 +0530

    Create 3. Bulls and Cows.java

---
+++ Microsoft/3. Bulls and Cows.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        //HashMap<Character,Character> hm = new HashMap<>();
+        int numBulls = 0;
+        int numCows = 0;
+        int numbers[] = new int[10];
+        for(int i=0;i<secret.length();i++){
+            
+            if(secret.charAt(i) == guess.charAt(i)){
+                numBulls++;
+                
+            }
+            else{
+                int s = Character.getNumericValue(secret.charAt(i));
+                int g = Character.getNumericValue(guess.charAt(i));
+                if(numbers[s] < 0){
+                    numCows++;
+                }
+                if(numbers[g] > 0){
+                    numCows++;
+                }
+                numbers[s]++;
+                numbers[g]--;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.valueOf(numBulls));
+        sb.append("A");
+        sb.append(Integer.valueOf(numCows));
+        sb.append("B");
+        return sb.toString();
+    }
+}
